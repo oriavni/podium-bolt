500534243b62311322fb7bf0d11f0b7f93ae43e1
oriavni <108334185+oriavni@users.noreply.github.com>
2025-03-05 14:25:22 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,27 +6,28 @@ const nextConfig = {
   images: { unoptimized: true },
   output: 'export',
   webpack: (config, { dev }) => {
-    // Add Babel configuration
-    config.module.rules.push({
-      test: /\.js$/,
-      use: [
-        {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-private-methods', '@babel/plugin-proposal-class-properties']
-          }
+  config.resolve.fallback = { crypto: false }; // Add this line
+  
+  // Existing Babel configuration
+  config.module.rules.push({
+    test: /\.js$/,
+    use: [
+      {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-private-methods', '@babel/plugin-proposal-class-properties']
         }
-      ]
-    });
+      }
+    ]
+  });
 
-    // Disable cache in development to prevent issues
-    if (dev) {
-      config.cache = false;
-    }
-
-    return config;
+  // Disable cache in development
+  if (dev) {
+    config.cache = false;
   }
-};
+
+  return config;
+}
 
 module.exports = nextConfig;
